abffb8974121084f549e31b1f46f04b731723cec
loganmiller001 <loganmiller001@live.com>
2018-07-17 10:16:22 -0500

    Adding untracked files from local repo.

---
+++ LINQProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+             var lettersCount = words.Where(w => w.Contains("th")).Count();
+            //    foreach (var letters in words)
+            //{
+            //    Console.WriteLine("Words with the letters 'th' are: " + lettersCount);
+            //}
+            Console.WriteLine("Words with the letters 'th' are: " + lettersCount);
+            Console.ReadKey();
+            
+        }
+    }
+}
