5e7561eda782f0b54db27fc15560455faf9b27ed
loganmiller001 <loganmiller001@live.com>
2018-07-17 14:38:18 -0500

    Completed problem4.

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -20,13 +20,36 @@ namespace LINQProblems
             //Console.ReadKey();
 
             //Problem2
-            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            //List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
 
-            var justOneName = names.Select(n => n).Distinct().ToList();
-            //List<string> names2 = names.Distinct().ToList();
-            foreach (var name in justOneName)
+            //var justOneName = names.Select(n => n).Distinct().ToList();
+            ////List<string> names2 = names.Distinct().ToList();
+            //foreach (var name in justOneName)
+            //{
+            //    Console.WriteLine("{0}", name);
+            //}
+            //Console.ReadKey();
+
+            //Problem3
+            //List<string> classGrades = new List<string>
+            //{
+            //    "80, 100, 92, 89, 65",
+            //    "93, 91, 78, 84, 69",
+            //    "73, 88, 83, 99, 64",
+            //    "98, 100, 66, 74, 55"
+
+            //};
+
+            //Problem4
+
+            string str = "terrill";
+            List<string> charCounts = str.GroupBy(ch => ch).OrderBy(item => item.Key).Select(
+                                   item => item.Key + "=" + item.Count().ToString()).ToList();
+            
+            foreach (string s in charCounts)
             {
-                Console.WriteLine("{0}", name);
+                Console.WriteLine(s);
+                
             }
             Console.ReadKey();
         }
