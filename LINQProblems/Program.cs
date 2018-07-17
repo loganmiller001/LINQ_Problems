f6f66fb5d6b4945a82adde1bc145a090614de22f
loganmiller001 <loganmiller001@live.com>
2018-07-17 10:49:31 -0500

    Problem2 completed.

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -10,16 +10,25 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
-            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
 
-             var lettersCount = words.Where(w => w.Contains("th")).Count();
-            //    foreach (var letters in words)
-            //{
-            //    Console.WriteLine("Words with the letters 'th' are: " + lettersCount);
-            //}
-            Console.WriteLine("Words with the letters 'th' are: " + lettersCount);
+            //Problem1
+            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            // var lettersCount = words.Where(w => w.Contains("th")).Count();
+
+            //Console.WriteLine("Words with the letters 'th' are: " + lettersCount);
+            //Console.ReadKey();
+
+            //Problem2
+            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+
+            // var justOneName = names.Select(n => n).Distinct().ToList();
+            List<string> names2 = names.Distinct().ToList();
+            foreach (var name in names2)
+            {
+                Console.WriteLine("{0}", name);
+            }
             Console.ReadKey();
-            
         }
     }
 }
