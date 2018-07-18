08b241b73f54cafd82a45a1e2fdef81ec51fd05f
loganmiller001 <loganmiller001@live.com>
2018-07-18 07:28:37 -0500

    Finished problem3, all problems finished.

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -40,20 +40,55 @@ namespace LINQProblems
                 "98, 100, 66, 74, 55"
 
             };
+            List<double> studentAverage = new List<double>();
+            double classAverage;
+            
 
+            for (int i = 0; i < classGrades.Count; i++)
+            {
+                var usersGrades = classGrades[i].Split(',').Select(s => int.Parse(s)).ToList();
+                var ascendingOrder = usersGrades.OrderBy(j => j).ToList();
+                Console.WriteLine("Sort");
+                foreach(var item in ascendingOrder)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("");
+
+                var dropLowest = from lowestScore in ascendingOrder
+                                 where lowestScore > ascendingOrder.Min()
+                                 select lowestScore;
+                Console.WriteLine("Drop lowest");
+                foreach (var item in dropLowest)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("");
+                studentAverage.Add(dropLowest.Average());
+            }
+            Console.WriteLine("Individual Averages");
+            for (int k = 0; k < studentAverage.Count; k++)
+            {
+                Console.WriteLine(studentAverage[k]);
+            }
+            Console.WriteLine("Class Average");
+            classAverage = studentAverage.Average();
+            Console.WriteLine(classAverage);
+            Console.WriteLine("");
+            Console.ReadLine();
 
             ////Problem4
 
-            string str = "terrill";
-            List<string> charCounts = str.GroupBy(ch => ch).OrderBy(item => item.Key).Select(
-                                   item => item.Key + "=" + item.Count().ToString()).ToList();
+                //string str = "terrill";
+                //List<string> charCounts = str.GroupBy(ch => ch).OrderBy(item => item.Key).Select(
+                //                       item => item.Key + "=" + item.Count().ToString()).ToList();
 
-            foreach (string s in charCounts)
-            {
-                Console.WriteLine(s);
+                //foreach (string s in charCounts)
+                //{
+                //    Console.WriteLine(s);
 
-            }
-            Console.ReadKey();
+                //}
+                //Console.ReadKey();
 
         }
     }
